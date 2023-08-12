30ba6be360451bc2743730c31a44d08384de902a
thamuz24 <fallenangelsiv05082004@gmail.com>
2023-08-12 20:07:42 +0700

    diem va vong tron

---
+++ src/diem_va_vong_tron.java
@@ -0,0 +1,29 @@
+import java.awt.*;
+import java.util.List;
+
+public class diem_va_vong_tron {
+    public static int solve(List<List<Double>> point, List<Double> r) {
+        // Write your code here
+
+        int result = -1;
+        for (int i = 0; i < r.size(); i++) {
+            boolean isBig = true;
+            Double radius = r.get(i);
+
+            for (List<Double> childPoint : point) {
+                for (Double num : childPoint) {
+                    if (num > radius) {
+                        isBig = false;
+                        break;
+                    }
+                }
+            }
+
+            if (isBig) {
+                result = i;
+                break;
+            }
+        }
+        return result;
+    }
+}
