b3dec1cda11957cae9fb31d42f67bac956a4f84e
thamuz24 <fallenangelsiv05082004@gmail.com>
2023-08-12 20:10:07 +0700

    diem va vong tron

---
+++ src/ket_hop_chu_cai_cua_mot_so_dien_thoai.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ket_hop_chu_cai_cua_mot_so_dien_thoai {
+
+//    public static String solve(int n) {
+//        // Write your code here
+//        String[] listNumber2 = new String[] {"a","b","c"};
+//        String[] listNumber3 = new String[] {"d","e","f"};
+//        String[] listNumber4 = new String[] {"g","h","i"};
+//        String[] listNumber5 = new String[] {"j","k","l"};
+//        String[] listNumber6 = new String[] {"m","n","o"};
+//        String[] listNumber7 = new String[] {"p","q","r","s"};
+//        String[] listNumber8 = new String[] {"t","u","v"};
+//        String[] listNumber9 = new String[] {"w","x","y","z"};
+//
+//        String input = Integer.toString(n);
+//        List<String> result = new ArrayList<>();
+//        for (int i = input.length(); i > 0; i++) {
+//
+//        }
+//    }
+}
+
--- src/test.java
+++ src/test.java
@@ -3,42 +3,8 @@ import java.util.List;
 
 public class test {
     public static void main(String[] args) {
-        String s = "  2  -11  +13 123432  832 22 99";
-        List<Integer> list = getOddHeadNumbers(s);
-        for (int i : list) {
-            System.out.println(i);
-        }
     }
 
-    public static List<Integer> getOddHeadNumbers(String s) {
-        String trueInput = s.replaceAll("\\s+", " ");
 
-        char checkSpake = trueInput.charAt(0);
-        if (checkSpake == ' ') {
-            trueInput = new String(trueInput.getBytes(),1,trueInput.length()-1);
-        }
-
-        // Write your code here
-        String[] listNumber = trueInput.split(" ");
-        List<Integer> listOddNumber = new ArrayList<>();
-        for (String number : listNumber) {
-            char a = number.charAt(0);
-            int firstNumber;
-            if (a == '-' || a == '+') {
-                firstNumber = Integer.parseInt(new String(number.getBytes(), 0, 2));
-            } else {
-                firstNumber = Integer.parseInt (new String(number.getBytes(),0,1));
-            }
-
-            if ( firstNumber % 2 != 0) {
-                listOddNumber.add(Integer.parseInt(number));
-            }
-        }
-
-        if (listOddNumber.size() == 0) {
-            listOddNumber.add(-1);
-        }
-        return listOddNumber;
-    }
 
 }
